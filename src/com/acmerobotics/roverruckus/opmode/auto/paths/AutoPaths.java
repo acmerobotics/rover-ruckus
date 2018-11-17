cfebfd3476f2cbe7bc934e7637a50a2034583777
Kelly Muir <kellyrmuir@gmail.com>
2018-11-16 22:37:02 -0800

    auto paths

--- src/com/acmerobotics/roverruckus/opmode/auto/paths/AutoPaths.java
+++ src/com/acmerobotics/roverruckus/opmode/auto/paths/AutoPaths.java
@@ -1,9 +1,19 @@
-package com.acmerobotics.roverruckus.opmode.auto;
+package com.acmerobotics.roverruckus.opmode.auto.paths;
 
 import com.acmerobotics.roadrunner.path.Path;
+import com.acmerobotics.roverruckus.opmode.auto.Auto;
 
-public class AutoPaths {
+public abstract class AutoPaths {
 
-    public static Path landerToSampling
+    public abstract Path landerToSample ();
+
+    public abstract Path sampleToDepot ();
+
+    public abstract  Path depotToPark ();
+
+    public static AutoPaths getPaths(Auto.START_LOCATION startLocaiton) {
+        if (startLocaiton == Auto.START_LOCATION.CRATER) return new BasicCraterPaths();
+        else return new BasicDepotPaths();
+    }
 
 }
--- src/com/acmerobotics/roverruckus/opmode/auto/paths/BasicDepotPaths.java
+++ src/com/acmerobotics/roverruckus/opmode/auto/paths/BasicDepotPaths.java
@@ -1,4 +1,23 @@
 package com.acmerobotics.roverruckus.opmode.auto.paths;
 
-public class BasicDepotPaths {
+import com.acmerobotics.roadrunner.path.Path;
+import com.acmerobotics.roverruckus.opmode.auto.Auto;
+
+public class BasicDepotPaths extends AutoPaths{
+
+
+    @Override
+    public Path landerToSample(Auto.START_LOCATION start) {
+        return null;
+    }
+
+    @Override
+    public Path sampleToDepot(Auto.START_LOCATION start) {
+        return null;
+    }
+
+    @Override
+    public Path depotToPark(Auto.START_LOCATION start) {
+        return null;
+    }
 }
