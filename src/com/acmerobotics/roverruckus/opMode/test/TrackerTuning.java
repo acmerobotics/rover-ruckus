dee0bf3dd9df56746f8fb10e798e0ec3a7d3456c
Kelly Muir <kellyrmuir@gmail.com>
2019-02-17 15:52:34 -0800

    add tuning opbode

--- src/com/acmerobotics/roverruckus/opMode/test/TrackerTuning.java
+++ src/com/acmerobotics/roverruckus/opMode/test/TrackerTuning.java
@@ -1,4 +1,30 @@
 package com.acmerobotics.roverruckus.opMode.test;
 
-public class TrackerTuning {
+import android.os.Environment;
+import android.util.Log;
+
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roverruckus.robot.Robot;
+import com.acmerobotics.roverruckus.trajectory.TrajectoryBuilder;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import java.io.File;
+import java.io.FileWriter;
+
+public class TrackerTuning extends LinearOpMode {
+
+    @Override
+    public void runOpMode () {
+        Robot robot = new Robot(this, hardwareMap);
+
+        waitForStart();
+
+        while (!isStopRequested()) {
+            double theta = Math.PI / 2;
+            Vector2d start = robot.drive.getTrackingPositions();
+
+            robot.drive.followTrajectory(new TrajectoryBuilder(robot.drive.getCurrentEstimatedPose()).);
+        }
+
+    }
 }
